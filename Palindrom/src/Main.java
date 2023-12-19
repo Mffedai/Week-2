bae71968838b8ebd85ad5a2ab391808b276c67dc
Mffedai <mfatihfedai@gmail.com>
2023-12-19 22:02:10 +0300

    Palindrom Sayılar

--- Palindrom/src/Main.java
+++ Palindrom/src/Main.java
@@ -1,8 +1,31 @@
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static boolean isPalindrom (int number){
+        int temp = number, reverseNumber = 0, lastNumber, digit = 0;
+        double palindrom = 0;
 
+        while (temp % 10 != 0){
+            temp = temp / 10;
+            digit++;
+        }
+        temp=number;
+        for(int i = 0; i < digit ; i++){
+
+            palindrom += Math.pow(10,i) *(temp % 10);
+            temp/=10;
+            System.out.println(palindrom); //Get the result for code control
+        }
+        if(palindrom == number){
+             return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(4554));
 
     }
 }
